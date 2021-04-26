cb06c2e0801ebec9acd96277112630b61e220783
Ubuntu <ubuntu@ip-172-31-27-115.ec2.internal>
2021-04-26 19:24:34 +0000

    Added Area_Perimeter.py, basic math operators with python

---
+++ Area_Perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env/ Python3
+
+# Created by Malcolm Tompkins
+# Created on April 26, 2021
+#A program that calculates the area and perimeter of a 5 x 3 rectangle
+
+
+def main():
+    print("Dimensions of rectangle:")
+    print("5cm x 3cm\n")
+    print("Area is: {}cm^2".format(5*3))
+    print("Perimeter is: {}cm".format(2*5+3))
+
+if __name__ == "__main__":
+    main()
+
+    
